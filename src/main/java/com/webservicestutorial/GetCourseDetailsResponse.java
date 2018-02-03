542b2e793d36557eb64ad550d6cc4481fef41658
Alexander <aav94@yandex.ru>
2018-02-03 19:50:29 +0300

    init

---
+++ src/main/java/com/webservicestutorial/GetCourseDetailsResponse.java
@@ -0,0 +1,188 @@
+
+package com.webservicestutorial;
+
+import java.math.BigInteger;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="CourseDetails">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}integer"/>
+ *                   &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *                   &lt;element name="description" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "courseDetails"
+})
+@XmlRootElement(name = "GetCourseDetailsResponse", namespace = "http://someBookDetails.com/courses")
+public class GetCourseDetailsResponse {
+
+    @XmlElement(name = "CourseDetails", namespace = "http://someBookDetails.com/courses", required = true)
+    protected GetCourseDetailsResponse.CourseDetails courseDetails;
+
+    /**
+     * Gets the value of the courseDetails property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link GetCourseDetailsResponse.CourseDetails }
+     *     
+     */
+    public GetCourseDetailsResponse.CourseDetails getCourseDetails() {
+        return courseDetails;
+    }
+
+    /**
+     * Sets the value of the courseDetails property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link GetCourseDetailsResponse.CourseDetails }
+     *     
+     */
+    public void setCourseDetails(GetCourseDetailsResponse.CourseDetails value) {
+        this.courseDetails = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}integer"/>
+     *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+     *         &lt;element name="description" type="{http://www.w3.org/2001/XMLSchema}string"/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "id",
+        "name",
+        "description"
+    })
+    public static class CourseDetails {
+
+        @XmlElement(namespace = "http://someBookDetails.com/courses", required = true)
+        protected BigInteger id;
+        @XmlElement(namespace = "http://someBookDetails.com/courses", required = true)
+        protected String name;
+        @XmlElement(namespace = "http://someBookDetails.com/courses", required = true)
+        protected String description;
+
+        /**
+         * Gets the value of the id property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link BigInteger }
+         *     
+         */
+        public BigInteger getId() {
+            return id;
+        }
+
+        /**
+         * Sets the value of the id property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link BigInteger }
+         *     
+         */
+        public void setId(BigInteger value) {
+            this.id = value;
+        }
+
+        /**
+         * Gets the value of the name property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * Sets the value of the name property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setName(String value) {
+            this.name = value;
+        }
+
+        /**
+         * Gets the value of the description property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getDescription() {
+            return description;
+        }
+
+        /**
+         * Sets the value of the description property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setDescription(String value) {
+            this.description = value;
+        }
+
+    }
+
+}
---
+++ src/main/java/com/webservicestutorial/endpoint/CourseDetailsEndpoint.java
@@ -0,0 +1,8 @@
+package com.webservicestutorial.endpoint;
+
+/**
+ * Created by Admin on 03.02.2018.
+ */
+public class CourseDetailsEndpoint {
+
+}
